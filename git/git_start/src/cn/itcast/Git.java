9459928240e5b3adce24ea5c3a10717f8fe4dcd6
Willis <2901340253@qq.com>
2018-10-28 19:01:22 +0800

    add project to resipotory

---
+++ git/git_start/src/cn/itcast/Git.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("hello git........");
+        System.out.println("hello git........");
+        System.out.println("hello git........");
+        System.out.println("hello git........");
+        System.out.println("hello git........");
+        System.out.println("hello git........");
+
+    }
+}
